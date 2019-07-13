b1108e3b641e82f7b7c78403890047f033dd2ba1
kosten00 <ylkahu@gmail.com>
2019-07-13 12:08:06 +0700

    vector task done fro the first time

--- Vector/src/ru/academits/vasilev/vector/Vector.java
+++ Vector/src/ru/academits/vasilev/vector/Vector.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class Vector {
     private int n;
     private double[] array;
-    private Vector vector;
 
     public Vector(int n) {
         if (n <= 0) {
@@ -45,7 +44,7 @@ public class Vector {
     }
 
     public int getSize() {
-        return n;
+        return this.n;
     }
 
     public Vector plusVector(Vector vector) {
@@ -54,6 +53,7 @@ public class Vector {
             double[] tempArray = this.array.length < vector.array.length ? this.array : vector.array;
 
             Vector tempVector = new Vector(tempLength, tempArray);
+
             for (int i = 0; i < tempVector.array.length; i++) {
                 tempVector.array[i] += vector.array[i];
             }
@@ -71,6 +71,7 @@ public class Vector {
             double[] tempArray = this.array.length < vector.array.length ? this.array : vector.array;
 
             Vector tempVector = new Vector(tempLength, tempArray);
+
             for (int i = 0; i < tempVector.array.length; i++) {
                 tempVector.array[i] -= vector.array[i];
             }
@@ -82,6 +83,73 @@ public class Vector {
         return new Vector(this.n, this.array);
     }
 
+    public Vector multiply(double number) {
+        for (int i = 0; i < this.array.length; i++) {
+            this.array[i] *= number;
+        }
+        return new Vector(this.n, this.array);
+    }
+
+    public Vector revert() {
+        for (int i = 0; i < this.array.length; i++) {
+            this.array[i] *= -1;
+        }
+        return new Vector(this.n, this.array);
+    }
+
+    public double getLength() {
+        double sum = 0;
+
+        for (int i = 0; i < this.array.length; i++) {
+            sum += this.array[i] * this.array[i];
+        }
+        return Math.sqrt(sum);
+    }
+
+    public Vector setElementByIndex(int index, double element) {
+        if (index >= this.array.length) {
+            throw new IllegalArgumentException();
+        }
+        this.array[index] = element;
+
+        return new Vector(n, this.array);
+    }
+
+    public double getElementByIndex(int index) {
+        if (index >= this.array.length) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.array[index];
+    }
+
+    public static Vector vectorsSum(Vector first, Vector second) {
+        first.plusVector(second);
+        return first;
+    }
+
+    public static Vector vectorsSubtraction(Vector first, Vector second) {
+        first.minusVector(second);
+        return first;
+    }
+
+    public static Vector vectorsMultiply(Vector first, Vector second) {
+        if (first.array.length != second.array.length) {
+            int tempLength = first.array.length < second.array.length ? second.array.length : first.array.length;
+            double[] tempArray = first.array.length < second.array.length ? first.array : second.array;
+
+            Vector tempVector = new Vector(tempLength, tempArray);
+
+            for (int i = 0; i < tempVector.array.length; i++) {
+                tempVector.array[i] *= second.array[i];
+            }
+            return tempVector;
+        }
+        for (int i = 0; i < first.array.length; i++) {
+            first.array[i] *= second.array[i];
+        }
+        return new Vector(first.n, first.array);
+    }
 
     @Override
     public String toString() {
