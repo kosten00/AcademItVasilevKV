3110663819bacc390b7b6b755b9c336f06d526e1
kosten00 <ylkahu@gmail.com>
2019-07-14 20:47:06 +0700

    range work on mistakes

--- Range/src/ru/academits/vasilev/range/Range.java
+++ Range/src/ru/academits/vasilev/range/Range.java
@@ -22,27 +22,57 @@ public class Range {
         return from <= x && to >= x;
     }
 
-    public Range getRangesIntersection(double from, double to) {
-        if (this.to < from) {
-            return null;
-        } else {
-            return new Range(this.from > from ? this.from : from, to < this.to ? to : this.to);
+    public Range getRangesIntersection(Range range) {
+        if (range.from < this.from && this.to < range.to) {
+            return new Range(this.from, this.to);
         }
+
+        if (this.from < range.from && this.to > range.to) {
+            return new Range(range.from, range.to);
+        } else {
+
+            if (this.to >= range.from) {
+                return new Range(range.from, this.to);
+            }
+
+            if (range.to >= this.from) {
+                return new Range(this.from, range.to);
+            }
+        }
+        return null;
     }
 
-    public Range[] getRangesSum(double from, double to) {
+
+    public Range[] uniteRanges(Range range) {
         if (this.to < from) {
             return new Range[]{new Range(this.from, this.to), new Range(from, to)};
         }
         return new Range[]{new Range(this.from, to)};
     }
 
-    public Range[] getRangesQuotient(double from, double to) {
-        if (new Range(this.from, this.to).getRangesIntersection(from, to) != null) {
-            return new Range[]{new Range(this.from, from)};
-        } else {
-            return new Range[]{new Range(this.from, this.to), new Range(from, to)};
+    public Range[] getRangesDifference(Range range) {
+        if (this.getRangesIntersection(range) == null) {
+            return new Range[]{this, range};
         }
+
+        if (range.from < this.from && range.to > this.to) {
+            return new Range[]{new Range(range.from, this.from), new Range(this.to, range.to)};
+        }
+
+        if (this.from < range.from && this.to > range.to) {
+            return new Range[]{new Range(this.from, range.from), new Range(range.to, this.to)};
+        }
+
+        if (this.from < range.from) {
+            return new Range[]{new Range(this.from, range.from)};
+        }
+
+        if (range.from < this.from) {
+            return new Range[]{new Range(range.from, this.from)};
+        }
+
+        return new Range[]{this};
+
     }
 
     public void setFrom(double from) {
--- Range/src/ru/academits/vasilev/range/RangeMain.java
+++ Range/src/ru/academits/vasilev/range/RangeMain.java
@@ -3,14 +3,16 @@ package ru.academits.vasilev.range;
 import java.util.Scanner;
 
 public class RangeMain {
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Range range1 = new Range(1, 7);
+        Range range1 = new Range(1, 5);
         range1.print();
         System.out.println("First range have length of " + range1.getLength());
 
-        Range range2 = new Range(8, 11);
+        Range range2 = new Range(5, 7);
         range2.print();
         System.out.println("Second range have length of " + range2.getLength());
 
@@ -29,20 +31,20 @@ public class RangeMain {
         }
 
 
-        if (range1.getRangesIntersection(range2.getFrom(), range2.getTo()) == null) {
+        if (range1.getRangesIntersection(range2) == null) {
             System.out.println("Ranges don't intersect");
         } else {
             System.out.print("Ranges intersect in ");
-            range1.getRangesIntersection(range2.getFrom(), range2.getTo()).print();
+            range1.getRangesIntersection(range2).print();
         }
 
         System.out.println("Ranges sum: ");
-        for (Range e : range1.getRangesSum(range2.getFrom(), range2.getTo())) {
+        for (Range e : range1.uniteRanges(range2)) {
             e.print();
         }
 
         System.out.println("Ranges quotient: ");
-        for (Range e : range1.getRangesQuotient(range2.getFrom(), range2.getTo())) {
+        for (Range e : range1.getRangesDifference(range2)) {
             e.print();
         }
     }
