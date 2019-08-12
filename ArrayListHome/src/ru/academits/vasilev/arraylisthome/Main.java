70be51f846a882fcbc5932f241454b80bfe6e3a9
Константин <ylkahu@gmail.com>
2019-08-12 16:26:13 +0700

    ArrayListHome task added

--- ArrayListHome/src/ru/academits/vasilev/arraylisthome/Main.java
+++ ArrayListHome/src/ru/academits/vasilev/arraylisthome/Main.java
@@ -1,5 +1,7 @@
 package ru.academits.vasilev.arraylisthome;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -9,16 +11,14 @@ public class Main {
         ArrayList<Integer> oddNumbersList = new ArrayList<>();
         ArrayList<Integer> numbersNotRepeatedList = new ArrayList<>();
 
-        try (Scanner scanner = new Scanner("some_file.txt")) {
-            String fromFile;
-
-            while ((fromFile = scanner.nextLine()) != null) {
-                oddNumbersList.add(Integer.parseInt(fromFile));
+        try (Scanner scanner = new Scanner(new FileInputStream("C:\\Users\\я\\IdeaProjects\\AcademItVasilevKV\\ArrayListHome\\src\\ru\\academits\\vasilev\\arraylisthome\\some_file.txt"))) {
+            while (scanner.hasNextLine()) {
+                oddNumbersList.add(Integer.parseInt(scanner.next()));
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
 
-        System.out.println();
-
-
+        System.out.println(oddNumbersList);
     }
 }
