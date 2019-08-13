56ba1ba1f5ba27847e3c80854cfb211dd2624de3
Константин <ylkahu@gmail.com>
2019-08-13 10:03:52 +0700

    ArrayListHome task work on

--- ArrayListHome/src/ru/academits/vasilev/arraylisthome/Main.java
+++ ArrayListHome/src/ru/academits/vasilev/arraylisthome/Main.java
@@ -19,14 +19,45 @@ public class Main {
         System.out.println(list1);
 
         for (int i = 0; i < list1.size(); i++) {
-            if (list1.get(i) % 2 == 0) {
-                list1.remove(i);
-                i--;
+            if (list1.get(i) % 2 != 0 || list1.get(i) == 0) {
+                continue;
             }
+            list1.remove(i);
+            i--;
         }
         System.out.println(list1);
 
-        ArrayList<Integer> list2 = new ArrayList<>(list1);
+        ArrayList<Integer> list2 = new ArrayList<>();
 
+        try (Scanner scanner = new Scanner(new FileInputStream("C:\\Users\\я\\IdeaProjects\\AcademItVasilevKV\\ArrayListHome\\src\\ru\\academits\\vasilev\\arraylisthome\\some_file.txt"))) {
+            while (scanner.hasNextLine()) {
+                list2.add(Integer.parseInt(scanner.nextLine()));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println(list2);
+
+        ArrayList<Integer> list3 = new ArrayList<>();
+
+        for (int i = 0; i < list2.size(); i++) {
+            boolean duplicated = false;
+
+            for (int j = 0; j < i; j++) {
+                if (list2.get(j).equals(list2.get(i))) {
+                    duplicated = true;
+
+                    break;
+                }
+            }
+
+            if (duplicated) {
+                continue;
+            }
+
+            list3.add(list2.get(i));
+        }
+
+        System.out.println(list3);
     }
 }
