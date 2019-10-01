ccf83392caf14b75b7baba64add783cbb685d9b4
kosten00 <ylkahu@gmail.com>
2019-10-01 17:33:20 +0700

    hashtable work started

--- HashTable/src/ru.academits.vasilev.hashtable/main/MyHashTableMain.java
+++ HashTable/src/ru.academits.vasilev.hashtable/main/MyHashTableMain.java
@@ -1,10 +1,18 @@
 package ru.academits.vasilev.hashtable.main;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Hashtable;
 
 public class MyHashTableMain {
 
-    Hashtable hashtable = new Hashtable();
+    public static void main(String[] args) {
+        int[] arr = {0,1,5, 9, 0 , 77, 694967467};
 
+        int i = Math.abs(arr.hashCode() % 10);
+
+
+        System.out.println(i);
+    }
 
 }
