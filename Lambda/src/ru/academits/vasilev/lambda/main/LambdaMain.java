eb59f850520c0c669bdd03fd58a48418c588a423
Константин <ylkahu@gmail.com>
2019-10-18 15:48:03 +0700

    lambda sent to first check

--- Lambda/src/ru/academits/vasilev/lambda/main/LambdaMain.java
+++ Lambda/src/ru/academits/vasilev/lambda/main/LambdaMain.java
@@ -2,36 +2,12 @@ package ru.academits.vasilev.lambda.main;
 
 import ru.academits.vasilev.lambda.Person;
 
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.function.Function;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 import java.util.stream.Stream;
 
-import static java.util.Comparator.*;
-
-/*
-Создать класс Person с полями имя и возраст. Сделать конструктор,
-который принимает эти параметры. Сделать геттеры для полей
-• В main создать список из нескольких людей
-• При помощи лямбда-функций:
-• А) получить список уникальных имен
-• Б) вывести список уникальных имен в формате:
-Имена: Иван, Сергей, Петр.
-• В) получить список людей младше 18, посчитать для них средний
-возраст
-• Г) при помощи группировки получить Map, в котором ключи –
-имена, а значения – средний возраст
-• Д) получить людей, возраст которых от 20 до 45, вывести в консоль
-их имена в порядке убывания возраста
-Задача 2
-• Создать бесконечный поток корней чисел. С консоли
-прочитать число – сколько элементов нужно вычислить,
-затем – распечатать эти элементы
-• * Попробовать реализовать бесконечный поток чисел
-Фиббоначчи
- */
+import static java.util.stream.Collectors.averagingInt;
 
 public class LambdaMain {
     public static void main(String[] args) {
@@ -45,21 +21,27 @@ public class LambdaMain {
         personList.add(new Person("Витя", 5));
         personList.add(new Person("Глаша", 19));
 
-        //personList.stream().filter(person -> person.getName());
-
-        //LinkedList<Person> list = personList.stream().distinct();
-
-        //System.out.println(personList.stream().distinct().collect(Collectors.toList()));
-
-
         //Creating list of unique names from personList.
-        LinkedList<String> names = new LinkedList<>();
         Stream<String> stream = personList.stream().map(Person::getName).distinct();
-        List<String> uniqueNames = stream.collect(Collectors.toList());
-        uniqueNames.forEach(System.out::println);
+        System.out.println("Names: " + stream.collect(Collectors.toList()));
 
+        //Creating list of persons younger than 18, count their average age:
+        Stream<Person> stream1 = personList.stream().filter(p -> p.getAge() < 18);
+        List<Person> youngerEighteen = stream1.collect(Collectors.toList());
+        System.out.println(youngerEighteen.stream().mapToInt(Person::getAge).average());
 
+        //Get Map with names as keys and average age as value.)
+        Map<String, Double> map = personList.stream().collect(Collectors.groupingBy(Person::getName, averagingInt(Person::getAge)));
+        map.forEach((name, age) -> System.out.printf("name: %s; average age: %s; ", name, age));
 
+        System.out.println();
+        //Get persons aged 20-45 y.o., print to the console their names in decreasing order:
+        personList.stream().filter(person -> person.getAge() <= 45).filter(person -> person.getAge() >= 20).sorted((p1, p2) -> p2.getAge() - p1.getAge()).forEach(System.out::println);
 
+        //Create infinite stream of square roots of numbers:
+        System.out.println("Input number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        DoubleStream.iterate(121, Math::sqrt).limit(number).forEach(System.out::println);
     }
 }
