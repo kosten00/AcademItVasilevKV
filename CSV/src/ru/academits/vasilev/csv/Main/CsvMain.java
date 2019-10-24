9f79f62bb72539ae88caca6f61484c38a7b44dc1
Константин <ylkahu@gmail.com>
2019-10-24 13:28:12 +0700

    csv work on

--- CSV/src/ru/academits/vasilev/csv/Main/CsvMain.java
+++ CSV/src/ru/academits/vasilev/csv/Main/CsvMain.java
@@ -29,87 +29,126 @@ public class CsvMain {
         </html> */
 
 
-    public static String checkForKeySymbol(int c) {
-        if (Character.toString(c).equals(",")) {
+    public static String checkForKeySymbol(String s) {
+        if (s.equals(",")) {
             return "comma";
         }
 
-        if (Character.toString(c).equals("\"")) {
+        if (s.equals("\"")) {
             return "quotes";
         }
 
-        if (Character.toString(c).equals("\n")) {
+        if (s.equals("\n")) {
             return "newline";
         }
 
-        return Character.toString(c);
+        return s;
     }
-
 
     private static void swCaseCsvConverter(String inputCsvFile, String outputHtmlFile) throws IOException {
         try (PrintWriter writer = new PrintWriter(outputHtmlFile);
              FileReader reader = new FileReader(inputCsvFile)) {
 
             int c;
-            boolean quotesOpened = false;
-            boolean commaPutted = false;
-
-            boolean startedTableRow = false;
-            boolean lastActionTableDetailMarked = false;
-
-            int quotesCount = 0;
 
             writer.print("<!DOCTYPE html><head><meta charset=\"UTF-8\"><body><table><tr><td>");
 
-
             boolean commaLastSign = false;
-            boolean newLineLastSign = false;
+            boolean newLineLastSign = true;
+            boolean quotesLastSign = false;
+            boolean quotesOpened = false;
 
             while ((c = reader.read()) != -1) {
-                if (c == 0) {
-                    quotesOpened = false;
-                    commaLastSign= false;
-                    newLineLastSign = false;
-                }
-
-                switch (checkForKeySymbol(c)) {
+                switch (checkForKeySymbol(Character.toString(c))) {
                     case ("comma"):
-                        //Два условия: если внутри ковычек или снаружи.
-                        if (quotesOpened) {
+                        System.out.println("\n" + " char catch in case: " + Character.toString(c) + " ");
+                        System.out.println(" got comma: ");
+
+                        if (quotesOpened && !quotesLastSign) {
+
+                            System.out.println(" quotes are opened and quotes not last sign ");
+
                             writer.print((char) c);
+
+                            System.out.println(" printing comma " + Character.toString(c));
+                            quotesOpened = false;
+
+                            System.out.println(" setting quotes closed ");
                             break;
                         }
-                        commaLastSign = true;
+
+                        System.out.println(" quotes opened status: " + quotesOpened + " quotes last sign status: " + quotesLastSign);
                         writer.print("</td><td>");
-                        quotesOpened = false;
+
+                        if (quotesLastSign) {
+                            quotesOpened = false;
+                            System.out.println(" quotes was last sign before comma, closing quotes, going further this case ");
+                        }
+
+                        commaLastSign = true;
+
+                        System.out.println(" setting comma as last sign ");
+                        System.out.println(" closing and opening details ");
+
                         break;
                     case ("quotes"):
-                        //Если после запятой, то ковычки открываются
-                        //Если ковычки отркыты, то закрыть
-                        if (commaLastSign || newLineLastSign) {
+                        System.out.println("\n" + " char catch in case: " + Character.toString(c) + " ");
+                        System.out.println(" got quotes ");
+
+                        if ((commaLastSign || newLineLastSign) && !quotesOpened) {
+                            System.out.println(" setting quotes last sign, setting quotes opened, reason: quotes not opened, comma - " + commaLastSign + ", new line - " + newLineLastSign + " ");
                             quotesOpened = true;
+                            quotesLastSign = true;
+                            break;
+                        }
+                        if (!quotesLastSign) {
+                            quotesLastSign = true;
+                            System.out.println(" last sign was not quotes, matching quotes as last sign, not printing quotes breaking next character ");
+                            break;
+                        }
+                        if (quotesLastSign /*&& !quotesOpened*/) {
+                            System.out.println(" quotes was last sing, printing quotes,  setting quotes last sign fals, breaking to next char ");
+                            writer.print((char) c);
+                            quotesLastSign = false;
                             break;
                         }
                         writer.print((char) c);
+                        quotesLastSign = true;
+                        System.out.println(" printing quotes" + Character.toString(c) + "setting quotes last sign true ");
                         break;
                     case ("newline"):
+                        System.out.println("\n" + " char catch in case: " + Character.toString(c) + " ");
+                        System.out.println(" got new line ");
+
                         if (quotesOpened) {
+                            System.out.println(" quotes are opened, printing /br ");
                             writer.print("<br/>");
                             break;
                         }
+                        System.out.println(" quotes are closed, printing tdtr, setting new line last sign as true ");
                         writer.print("</td></tr><tr><td>");
+
+
+                        if (quotesOpened) {
+                            System.out.println(" quotes are opened, closing quotes ");
+
+                            quotesOpened = false;
+                        }
+
                         newLineLastSign = true;
                         break;
                     default:
                         writer.print((char) c);
                         commaLastSign = false;
+                        newLineLastSign = false;
+                        quotesLastSign = false;
+                        System.out.print(Character.toString(c)/* + " last signs to false; quotes opened? - " + quotesOpened*/);
                 }
             }
 
             writer.print("</td></tr></table></body></head>");
         }
     }
-
 
     public static void main(String[] args) throws IOException {
         String inputCsvFile = "FileForCsvTask.csv";
