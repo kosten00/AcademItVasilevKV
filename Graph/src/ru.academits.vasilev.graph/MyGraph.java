d275c55b2c4cdfe33d6cbd8e70e02dc4e97d12e1
Константин <ylkahu@gmail.com>
2019-10-29 15:53:12 +0700

    graph work on

--- Graph/src/ru.academits.vasilev.graph/MyGraph.java
+++ Graph/src/ru.academits.vasilev.graph/MyGraph.java
@@ -1,10 +1,22 @@
 package ru.academits.vasilev.graph;
 
+import java.util.Stack;
+
 public class MyGraph<T> {
     private int maxVertexes;
     private int vertexesCount;
-    private Vertex<T> vertexes[];
+    private Vertex<T>[] vertexes;
     private int[][] graphMatrix;
+
+    private int getVertexToVisit(int i) {
+        for (int j = 0; j < vertexesCount; j++) {
+            if (graphMatrix[i][j] == 1 && !vertexes[j].isVisited()) {
+                return j;
+            }
+        }
+
+        return -1;
+    }
 
     public MyGraph(int graphSize) {
         this.maxVertexes = graphSize;
@@ -25,5 +37,27 @@ public class MyGraph<T> {
     public void addEdge(int start, int end) {
         graphMatrix[start][end] = 1;
         graphMatrix[end][start] = 1;
+    }
+
+    public void visitInDepth() {
+        vertexes[0].setVisited(true);
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        while (!stack.isEmpty()) {
+            int v = getVertexToVisit(stack.peek());
+
+            if (v == -1) {
+                stack.pop();
+            } else {
+                vertexes[v].setVisited(true);
+                stack.push(v);
+            }
+        }
+
+        for (int i = 0; i < vertexesCount; i++) {
+            vertexes[i].setVisited(false);
+        }
     }
 }
