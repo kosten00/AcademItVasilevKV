a9b121d4e9c72296d4070106c21e295031677568
Константин <ylkahu@gmail.com>
2019-10-31 12:43:14 +0700

    added overrided toString, equals, hashCode to scales

--- TemperatureConversion/src/ru.academits.vasilev.temperature/main/Launcher.java
+++ TemperatureConversion/src/ru.academits.vasilev.temperature/main/Launcher.java
@@ -6,6 +6,19 @@ import ru.academits.vasilev.temperature.scales.Celsius;
 import ru.academits.vasilev.temperature.scales.Fahrenheit;
 import ru.academits.vasilev.temperature.scales.Kelvin;
 
+/*
+3. В конце имени классов шкал лучше добавить слово Scale
+4. if (from.equals(to))
+У шкал не переопределен equals
+5. Если нужно перевести строку, то нужно переносить вместе с точкой:
+http://joxi.ru/V2VekyKhd6v9Em
+6. Есть ошибка в переводе температур.
+Например, неверно переводит из Кельвина в Цельсия.
+И переменной list лучше дать более информативное имя
+7. При проверке корректности ввода числа лучше всего просто использовать методы, например, Double.parseDouble.
+Если парсится, то все верно
+ */
+
 public class Launcher {
     public static void main(String[] args) {
         new Controller(new Scale[]{new Celsius("celsius"),
--- TemperatureConversion/src/ru.academits.vasilev.temperature/scales/Scale.java
+++ TemperatureConversion/src/ru.academits.vasilev.temperature/scales/Scale.java
@@ -1,6 +1,8 @@
 package ru.academits.vasilev.temperature.scales;
 
 
+import java.util.Objects;
+
 public abstract class Scale {
     private String name;
 
@@ -15,4 +17,20 @@ public abstract class Scale {
     public abstract double convertFromCelsius(double temperature);
 
     public abstract double convertToCelsius(double temperature);
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Scale scale = (Scale) o;
+        return name.equals(scale.name);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 37;
+        int hash = 1;
+        hash = prime * hash + name.hashCode();
+        return hash;
+    }
 }
