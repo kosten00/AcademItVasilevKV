6dc9e2417249682394fe9b4875a52f245b5f1cc5
Константин <ylkahu@gmail.com>
2019-11-01 11:07:16 +0700

    csv fixed

---
+++ CSV/src/ru/academits/vasilev/csv/CSV.java
@@ -0,0 +1,132 @@
+package ru.academits.vasilev.csv;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CSV {
+    private static final char START_OF_FILE_STREAM = 65279;
+    private static final char COMMA = 44;
+    private static final char QUOTES = 34;
+    private static final char NEXT_LINE = 10;
+    private static final char CARRIAGE_RETURN = 13;
+    private static final char LESS_SIGN = 60;
+    private static final char GREATER_SIGN = 62;
+    private static final char AMP_SIGN = 38;
+
+    private static boolean commaLastCharacter = false;
+    private static boolean quotesOpened = false;
+    private static boolean quotesInOpenedQuotesLastCharacter = false;
+    private static boolean lineSeparatorLastCharacter = false;
+    private static boolean carriageReturnLastCharacter = false;
+    private static boolean lineSeparatorAfterCarriageReturnLastCharacter = false;
+
+    public static void convertToCsv(String inputCsvFile, String outputHtmlFile) throws IOException {
+        try (PrintWriter writer = new PrintWriter(outputHtmlFile);
+             FileReader reader = new FileReader(inputCsvFile)) {
+
+            int c;
+
+            while ((c = reader.read()) != -1) {
+                switch (c) {
+                    case START_OF_FILE_STREAM:
+                        writer.print(START_OF_FILE_STREAM + "<!DOCTYPE html><html><meta charset=\"UTF-8\"><body><head><table><tr><td>");
+
+                        break;
+                    case COMMA:
+                        if (lineSeparatorAfterCarriageReturnLastCharacter) {
+                            writer.print("</td></tr><tr><td>");
+                            lineSeparatorAfterCarriageReturnLastCharacter = false;
+                        }
+
+                        if (!quotesInOpenedQuotesLastCharacter & quotesOpened) {
+                            writer.print(COMMA);
+
+                            break;
+                        }
+                        writer.print("</td><td>");
+
+                        if (quotesInOpenedQuotesLastCharacter) {
+                            quotesOpened = false;
+                            quotesInOpenedQuotesLastCharacter = false;
+                        }
+                        commaLastCharacter = true;
+
+                        break;
+                    case QUOTES:
+                        if (lineSeparatorAfterCarriageReturnLastCharacter) {
+                            writer.print("</td></tr><tr><td>");
+                            lineSeparatorAfterCarriageReturnLastCharacter = false;
+                        }
+
+                        if ((commaLastCharacter || lineSeparatorLastCharacter) && !quotesOpened) {
+                            quotesOpened = true;
+
+                            break;
+                        }
+
+                        if (!quotesInOpenedQuotesLastCharacter) {
+                            quotesInOpenedQuotesLastCharacter = true;
+
+                            break;
+                        }
+                        writer.print(QUOTES);
+
+                        quotesInOpenedQuotesLastCharacter = false;
+
+                        break;
+                    case NEXT_LINE:
+                        if (carriageReturnLastCharacter) {
+                            carriageReturnLastCharacter = false;
+                            lineSeparatorLastCharacter = false;
+                            lineSeparatorAfterCarriageReturnLastCharacter = true;
+                        }
+
+                        if (!quotesInOpenedQuotesLastCharacter & quotesOpened) {
+                            writer.print("</br>");
+                            break;
+                        }
+
+                        if (quotesInOpenedQuotesLastCharacter) {
+                            quotesOpened = false;
+                            quotesInOpenedQuotesLastCharacter = false;
+                        }
+                        lineSeparatorLastCharacter = true;
+
+                        break;
+                    case CARRIAGE_RETURN:
+                        carriageReturnLastCharacter = true;
+
+                        break;
+                    case LESS_SIGN:
+                        writer.print("&lt;");
+
+                        break;
+                    case GREATER_SIGN:
+                        writer.print("&gt;");
+
+                        break;
+                    case AMP_SIGN:
+                        writer.print("&amp;");
+
+                        break;
+                    default:
+                        if (lineSeparatorAfterCarriageReturnLastCharacter) {
+                            writer.print("</td></tr><tr><td>");
+                            lineSeparatorAfterCarriageReturnLastCharacter = false;
+                        }
+                        commaLastCharacter = false;
+                        lineSeparatorLastCharacter = false;
+                        quotesInOpenedQuotesLastCharacter = false;
+                        carriageReturnLastCharacter = false;
+
+                        writer.print((char) c);
+                }
+            }
+            writer.print("</td></tr></table></head></body></html>");
+        } catch (FileNotFoundException e) {
+            System.out.println("File was not found!");
+        }
+    }
+}
--- CSV/src/ru/academits/vasilev/csv/Main/CsvMain.java
+++ CSV/src/ru/academits/vasilev/csv/Main/CsvMain.java
@@ -1,5 +1,7 @@
 package ru.academits.vasilev.csv.Main;
 
+import ru.academits.vasilev.csv.CSV;
+
 import java.io.*;
 
 public class CsvMain {
@@ -8,12 +10,10 @@ public class CsvMain {
             System.out.println("Input and output file's names must be arguments of the program.");
         } else {
             try {
-                //CSV.convertToCsv(args[0], args[1]);
-                CSV.convertToCsv("in.csv", "out.html");
+                CSV.convertToCsv(args[0], args[1]);
             } catch (IOException ignored) {
                 System.out.println("Read/write error");
             }
-
         }
     }
 }
