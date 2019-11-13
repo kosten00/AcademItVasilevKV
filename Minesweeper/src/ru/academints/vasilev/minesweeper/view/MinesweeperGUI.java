85fa099079440a48b1ebcb2c0e82433d569b5b7a
Константин <ylkahu@gmail.com>
2019-11-13 17:31:07 +0700

    minesweeper JPanel testing

--- Minesweeper/src/ru/academints/vasilev/minesweeper/view/MinesweeperGUI.java
+++ Minesweeper/src/ru/academints/vasilev/minesweeper/view/MinesweeperGUI.java
@@ -8,23 +8,27 @@ public class MinesweeperGUI {
     private int boardSize;
     private int bombsCount;
 
+    int frameWidth = 555;
+    int frameHeight = 570;
+
     public MinesweeperGUI() {
         initVisibleBoard();
     }
 
-    //temporary starts just board with cells
     public void initVisibleBoard() {
         SwingUtilities.invokeLater(() -> {
-            new MinesweeperVisibleBoard();
+            initMainFrame();
         });
     }
 
     public void initMainFrame() {
         JFrame mainFrame = new JFrame("Minesweeper");
-        mainFrame.setSize(500, 500);
+        mainFrame.setSize(frameWidth, frameHeight);
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mainFrame.setLocationRelativeTo(null);
         mainFrame.setResizable(false);
         mainFrame.setVisible(true);
+
+        mainFrame.add(new MinesweeperVisibleBoard(frameWidth, frameHeight));
     }
 }
--- Minesweeper/src/ru/academints/vasilev/minesweeper/view/MinesweeperVisibleBoard.java
+++ Minesweeper/src/ru/academints/vasilev/minesweeper/view/MinesweeperVisibleBoard.java
@@ -5,37 +5,35 @@ import ru.academints.vasilev.minesweeper.model.MinesweeperBoardModel;
 import javax.swing.*;
 import java.awt.*;
 
-public class MinesweeperVisibleBoard extends JFrame {
+public class MinesweeperVisibleBoard extends JPanel {
     private MinesweeperBoardModel boardModel;
 
-    private int boardSize = 500;
+    private int width;
+    private int height;
 
     private int spaceBetweenCells = 5;
 
-    private int cellSize = 10;
+    private int cellSize = 50;
 
-    public MinesweeperVisibleBoard() {
-        boardModel = new MinesweeperBoardModel(5, 5);
-
-        setTitle("Board testing");
-        setSize(boardSize, boardSize);
-        setVisible(true);
-        setResizable(false);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    public MinesweeperVisibleBoard(int width, int height) {
+        this.width = width;
+        this.height = height;
     }
 
-    public void paint(Graphics g) {
-        g.setColor(Color.black);
-        //g.fillRect(0, 0, boardSize, boardSize);
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
-        //g.setColor(Color.white);
+        g.setColor(Color.black);
+        g.fillRect(0,0, width, height);
+        g.setColor(Color.white);
+
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                g.fillRect(spaceBetweenCells + i * cellSize, spaceBetweenCells + j * cellSize + cellSize, cellSize - spaceBetweenCells, cellSize - spaceBetweenCells);
+                g.fillRect(spaceBetweenCells + i * cellSize, spaceBetweenCells + j * cellSize, cellSize - spaceBetweenCells, cellSize - spaceBetweenCells);
 
-                //g.fillRect();
             }
         }
+
     }
 }
