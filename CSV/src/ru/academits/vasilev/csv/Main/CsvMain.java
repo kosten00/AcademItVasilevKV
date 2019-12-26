1018afc7852a97da3956b60898c7d709572e2bdb
Константин <ylkahu@gmail.com>
2019-12-26 13:40:15 +0700

    repo minor fixes

--- CSV/src/ru/academits/vasilev/csv/Main/CsvMain.java
+++ CSV/src/ru/academits/vasilev/csv/Main/CsvMain.java
@@ -8,13 +8,15 @@ public class CsvMain {
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Input and output file's names must be arguments of the program.");
-        } else {
-            try {
-                CsvToHtmlConverter converter = new CsvToHtmlConverter();
-                converter.convertToCsv(args[0], args[1]);
-            } catch (IOException ignored) {
-                System.out.println("Read/write error");
-            }
+
+            return;
         }
+        try {
+            CsvToHtmlConverter converter = new CsvToHtmlConverter();
+            converter.convertToHtml(args[0], args[1]);
+        } catch (IOException ignored) {
+            System.out.println("Read/write error");
+        }
+
     }
 }
